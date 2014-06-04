4410a3406cef1c28e026af67052e69e9964e17e0
Louw Swart <louw77@gmail.com>
2014-06-04 16:37:03 +1200

    resetthenet

---
+++ src/main/webapp/js/defenseleague.js
@@ -0,0 +1,12 @@
+window._idl = {};
+_idl.variant = "modal";
+(function() {
+  var idl = document.createElement('script');
+  idl.type = 'text/javascript';
+  idl.async = true;
+  idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://')
+      + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '')
+      + '&campaign=' + (_idl.campaign || '') + '&variant='
+      + (_idl.variant || 'modal');
+  document.getElementsByTagName('body')[0].appendChild(idl);
+})();
