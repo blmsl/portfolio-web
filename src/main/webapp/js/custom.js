29b190bffb36db4c7ee852d54d3894450e2c40de
Louw Swart <louw77@gmail.com>
2014-06-07 11:44:00 +1200

    Google Map tweaks

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -99,6 +99,9 @@ var mapMarkerDrawn = false;
           setTimeout(function() {
             marker = addMarker();
             google.maps.event.addListener(marker, 'click', toggleBounce);
+            if ($(window).width() > 1000) {
+              map.setZoom(11);
+            }
           }, 2500);
           mapMarkerDrawn = true;
         }
@@ -174,7 +177,7 @@ function elementInViewport(el) {
 function initialize() {
   var mapOptions = {
     center: wellington,
-    zoom: 11,
+    zoom: 10,
     scrollwheel: false,
     styles: [{featureType:'water',elementType:'geometry',stylers:[{color:'#00bdbd'}]},{featureType:'landscape.man_made',elementType:'geometry',stylers:[{color:'#f7f1df'}]},{featureType:'landscape.natural',elementType:'geometry',stylers:[{color:'#bde6ab'}]},{featureType:'landscape.natural.terrain',elementType:'geometry',stylers:[{visibility:'off'}]},{featureType:'poi.park',elementType:'geometry',stylers:[{color:'#abce83'}]},{featureType:'poi',elementType:'labels',stylers:[{visibility:'off'}]},{featureType:'poi.medical',elementType:'geometry',stylers:[{color:'#fbd3da'}]},{featureType:'poi.business',stylers:[{visibility:'off'}]},{featureType:'road',elementType:'geometry.stroke',stylers:[{visibility:'off'}]},{featureType:'road',elementType:'labels',stylers:[{visibility:'off'}]},{featureType:'road.highway',elementType:'geometry.fill',stylers:[{color:'#f5534b'}]},{featureType:'road.highway',elementType:'geometry.stroke',stylers:[{color:'#f5534b'}]},{featureType:'road.arterial',elementType:'geometry.fill',stylers:[{color:'#ff675f'}]},{featureType:'road.local',elementType:'geometry.fill',stylers:[{color:'black'}]},{featureType:'transit.station.airport',elementType:'geometry.fill',stylers:[{color:'#cfb2db'}]},{featureType:'transit.line',elementType:'geometry.fill',stylers:[{color:'#474d5d'}]},{featureType:'transit.line',elementType:'geometry.stroke',stylers:[{color:'#474d5d'}]}]
   };
