fc8c291379464788e42decc1fb20b62b708a6f5f
Louw Swart <louw77@gmail.com>
2014-06-06 21:33:45 +1200

    Styling of Google Map

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -5,7 +5,7 @@ var chart,
     timeoutMenuAnimate,
     map,
     marker;
-var wlgHarbour = new google.maps.LatLng(-41.280909, 174.850303);
+var wellington = new google.maps.LatLng(-41.284938, 174.762324);
 var eastbourne = new google.maps.LatLng(-41.291767, 174.897501);
 var skillChartDrawn = false;
 var mapMarkerDrawn = false;
@@ -173,7 +173,7 @@ function elementInViewport(el) {
 // Google maps
 function initialize() {
   var mapOptions = {
-    center: wlgHarbour,
+    center: wellington,
     zoom: 11,
     scrollwheel: false,
     styles: [{featureType:'water',elementType:'geometry',stylers:[{color:'#00bdbd'}]},{featureType:'landscape.man_made',elementType:'geometry',stylers:[{color:'#f7f1df'}]},{featureType:'landscape.natural',elementType:'geometry',stylers:[{color:'#bde6ab'}]},{featureType:'landscape.natural.terrain',elementType:'geometry',stylers:[{visibility:'off'}]},{featureType:'poi.park',elementType:'geometry',stylers:[{color:'#abce83'}]},{featureType:'poi',elementType:'labels',stylers:[{visibility:'off'}]},{featureType:'poi.medical',elementType:'geometry',stylers:[{color:'#fbd3da'}]},{featureType:'poi.business',stylers:[{visibility:'off'}]},{featureType:'road',elementType:'geometry.stroke',stylers:[{visibility:'off'}]},{featureType:'road',elementType:'labels',stylers:[{visibility:'off'}]},{featureType:'road.highway',elementType:'geometry.fill',stylers:[{color:'#f5534b'}]},{featureType:'road.highway',elementType:'geometry.stroke',stylers:[{color:'#f5534b'}]},{featureType:'road.arterial',elementType:'geometry.fill',stylers:[{color:'#ff675f'}]},{featureType:'road.local',elementType:'geometry.fill',stylers:[{color:'black'}]},{featureType:'transit.station.airport',elementType:'geometry.fill',stylers:[{color:'#cfb2db'}]},{featureType:'transit.line',elementType:'geometry.fill',stylers:[{color:'#474d5d'}]},{featureType:'transit.line',elementType:'geometry.stroke',stylers:[{color:'#474d5d'}]}]
