ddcfcdc868a35a05c749f4bc16ff473a841fef0f
Louw Swart <louw77@gmail.com>
2016-02-23 09:34:09 +1300

    Fixed banner size change on mobile scroll

--- src/header/components/header.ts
+++ src/header/components/header.ts
@@ -14,7 +14,6 @@ import {MENU_CONFIG, GRID_ROTATOR_CONFIG} from '../../shared/models/header/heade
 })
 export class HeaderComponent implements OnInit {
   public imageIds:Array<String>;
-  private _timeoutResize:any;
   private _previousWidth:number;
   private _previousHeight:number;
 
@@ -35,6 +34,26 @@ export class HeaderComponent implements OnInit {
     );
   }
 
+  initResizeListener() {
+    (($) => {
+      $(window).on('resize', _.bind(() => {
+        this.setBannerSize(this._previousWidth, this._previousHeight);
+      }, this));
+    })(jQuery);
+  }
+
+  initGridRotator(imageIds:Array<String>) {
+    this.imageIds = imageIds;
+    (($) => {
+      // For background slider
+      _.delay(_.bind(() => {
+        $('#ri-grid').gridrotator(GRID_ROTATOR_CONFIG);
+        this.setBannerSize(this._previousWidth, this._previousHeight);
+        this.initNavigation();
+      }, this), 250);
+    })(jQuery);
+  }
+
   initNavigation() {
     (($) => {
       $('.navbar-wrapper').stickUp(MENU_CONFIG);
@@ -51,40 +70,10 @@ export class HeaderComponent implements OnInit {
     })(jQuery);
   }
 
-  initGridRotator(imageIds:Array<String>) {
-    this.imageIds = imageIds;
-    (($) => {
-      // For background slider
-      _.delay(_.bind(() => {
-        $('#ri-grid').gridrotator(GRID_ROTATOR_CONFIG);
-        this.setBannerSize(this._previousWidth, this._previousHeight);
-        this.setDynamicCssValues();
-        this.initNavigation();
-      }, this), 250);
-    })(jQuery);
-  }
-
-  initResizeListener() {
-    (($) => {
-      $(window).on('resize', _.bind(() => {
-        this.setBannerSize(this._previousWidth, this._previousHeight);
-        this.setDynamicCssValues();
-        if (this._timeoutResize) {
-          clearTimeout(this._timeoutResize);
-        }
-        // wait half a second for resizing to stop before setting new sizes
-        this._timeoutResize = _.delay(_.bind(() => {
-          this._previousWidth = $(window).width();
-          this._previousHeight = $(window).innerHeight();
-        }, this), 500);
-      }, this));
-    })(jQuery);
-  }
-
   setBannerSize(previousWidth, previousHeight) {
     (($, previousWidth, previousHeight) => {
       var windowWidth = $(window).width(),
-        windowHeight = $(window).height(),
+        windowHeight = $(window).innerHeight(),
         widthChanged = previousWidth !== windowWidth,
         heightChanged = false;
       // mobile browsers ads about 60px to screen height when hiding address bar - ignore this
@@ -99,6 +88,9 @@ export class HeaderComponent implements OnInit {
           'width': windowWidth,
           'height': windowHeight - 60
         });
+        this.setDynamicCssValues();
+        this._previousWidth = windowWidth;
+        this._previousHeight = windowHeight;
       }
     })(jQuery, previousWidth, previousHeight);
   }
@@ -106,7 +98,7 @@ export class HeaderComponent implements OnInit {
   setDynamicCssValues() {
     (($) => {
       var bannerText = $('.banner-text');
-      bannerText.css('top', ((($(window).height() - bannerText.height()) / 2) - 63));
+      bannerText.css('top', ((($(window).innerHeight() - bannerText.height()) / 2) - 63));
     })(jQuery);
   }
 }
