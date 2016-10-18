aef885be32b1bf8df4bee5fde1d0c6d7be340c3f
Louw Swart <louw77@gmail.com>
2016-10-19 07:22:55 +1300

    Ensure 404 is sent

--- server/routes/fourohfour.js
+++ server/routes/fourohfour.js
@@ -3,5 +3,6 @@ let path = require('path')
 
 module.exports = (req, res) => {
   console.error('No resource matches: ' + req.url)
-  res.sendFile(path.resolve(__dirname, '../../app/404.html'))
+  res.status(404)
+    .sendFile(path.resolve(__dirname, '../../app/404.html'))
 }
