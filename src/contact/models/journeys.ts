bafc5cdf35026ed4f3f9d3c5044e1a8206b6f0a8
Louw Swart <louw77@gmail.com>
2016-10-21 19:41:37 +1300

    Updated dependencies and route info

--- src/contact/models/journeys.ts
+++ src/contact/models/journeys.ts
@@ -28,7 +28,8 @@ export const JOURNEYS: Array<Array<Airport>> = [
   [airports.AKL, airports.ZQN], [airports.AKL, airports.NSN], [airports.WLG, airports.TRG], [airports.WLG, airports.ROT],
   [airports.WLG, airports.CHC], [airports.AKL, airports.RAR], [airports.AKL, airports.NRT, airports.LHR],
   [airports.LHR, airports.ARN], [airports.BGO, airports.TRD], [airports.BOO, airports.SJV], [airports.TOS, airports.ARN],
-  [airports.LHR, airports.SIN, airports.SYD, airports.WLG], [airports.WLG, airports.TIU], [airports.WLG, airports.NSN]
+  [airports.LHR, airports.SIN, airports.SYD, airports.WLG], [airports.WLG, airports.TIU], [airports.WLG, airports.NSN],
+  [airports.WLG, airports.NPL]
 ];
 
 /**
@@ -36,6 +37,5 @@ export const JOURNEYS: Array<Array<Airport>> = [
  * @type {Airport[][]}
  */
 export const UPCOMING_JOURNEYS: Array<Array<Airport>> = [
-  [airports.WLG, airports.CBR, airports.SIN, airports.JNB],
-  [airports.WLG, airports.NPL]
+  [airports.WLG, airports.CBR, airports.SIN, airports.JNB]
 ];
