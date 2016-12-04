0c872beb59edf0898009c04b203fed948802d6ce
Louw Swart <louw77@gmail.com>
2016-12-04 20:20:26 +1300

    Updated journeys

--- src/contact/models/journeys.ts
+++ src/contact/models/journeys.ts
@@ -28,7 +28,7 @@ export const JOURNEYS: Array<Array<Airport>> = [
   [airports.WLG, airports.CHC], [airports.AKL, airports.RAR], [airports.AKL, airports.NRT, airports.LHR],
   [airports.LHR, airports.ARN], [airports.BGO, airports.TRD], [airports.BOO, airports.SJV], [airports.TOS, airports.ARN],
   [airports.LHR, airports.SIN, airports.SYD, airports.WLG], [airports.WLG, airports.TIU], [airports.WLG, airports.NSN],
-  [airports.WLG, airports.NPL],
+  [airports.WLG, airports.NPL], [airports.WLG, airports.CBR, airports.SIN, airports.JNB],
 ];
 
 /**
@@ -36,5 +36,5 @@ export const JOURNEYS: Array<Array<Airport>> = [
  * @type {Airport[][]}
  */
 export const UPCOMING_JOURNEYS: Array<Array<Airport>> = [
-  [airports.WLG, airports.CBR, airports.SIN, airports.JNB], [airports.WLG, airports.ZQN],
+  [airports.WLG, airports.ZQN],
 ];
